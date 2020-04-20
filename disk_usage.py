fefef7c41bf832fe126fa10b1fd2c708579fd16b
Rakesh K <rakesh.kg@zohocorp.com>
2020-04-20 22:29:43 +0530

    The arguments min_percent and min_absolute of check_disk_usage() has been switched.

--- disk_usage.py
+++ disk_usage.py
@@ -3,7 +3,7 @@
 import shutil
 import sys
 
-def check_disk_usage(disk, min_absolute, min_percent):
+def check_disk_usage(disk, min_percent, min_absolute):
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free/du.total
     gigabytes_free = du.free / 2**30
@@ -11,7 +11,7 @@ def check_disk_usage(disk, min_absolute, min_percent):
         return False
     return True
 
-if not check_disk_usage("/", 2, 10):
+if not check_disk_usage("/", 10, 2):
     print("ERROR : Not enough disk space.!")
     sys.exit(1)
 
