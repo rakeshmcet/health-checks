fc998b39afda3f28e054fabc658970f429b052f7
Rakesh K <rakesh.kg@zohocorp.com>
2020-04-20 22:34:00 +0530

    In check_disk_usage argument name changed.
    The disk usage function argument min_absolute was changed to min_gb.
    
    The new name is more suitable, as it really means the minimum disk size in gb

--- disk_usage.py
+++ disk_usage.py
@@ -3,11 +3,11 @@
 import shutil
 import sys
 
-def check_disk_usage(disk, min_absolute, min_percent):
+def check_disk_usage(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free/du.total
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return False
     return True
 
