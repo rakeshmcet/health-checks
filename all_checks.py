eeee99df83197d2ff8bc985a7d4b92dec254888e
Rakesh K <rakesh.kg@zohocorp.com>
2020-04-21 20:49:17 +0530

    Renamed function check_disk_usage to check_disk_full. Also added comments to give more information about the function

--- all_checks.py
+++ all_checks.py
@@ -8,9 +8,12 @@ def check_reboot():
     '''Returns True, if the computer has a pending reboot.'''
     return os.path.exists("/run/reboot-required")
 
-def check_disk_usage(disk, min_gb, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
+    ''' Return True if there isn't enough disk space, False otherwise. "
     du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
     percent_free = 100 * du.free/du.total
+    # Caalculat ehow many free gigabytes
     gigabytes_free = du.free / 2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
         return False
